2e20d8527f83258f79c6356c3fa21b27a6b81bc8
Kosuke Nagao <kosukengo@gmail.com>
2021-02-20 11:18:52 +0900

    swiftlintの警告修正

---
+++ Appdojo_Part07/MinusViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MinusViewController.swift
+//  Appdojo_Part07
+//
+//  Created by Kosuke Nagao on 2021/02/19.
+//
+
+import UIKit
+
+class MinusViewController: UIViewController {
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var resultLabel: UILabel!
+    @IBAction private func pushedButton(_ sender: Any) {
+        view.endEditing(true)
+        let value1 = Int(textField1.text!) ?? 0
+        let value2 = Int(textField2.text!) ?? 0
+        resultLabel.text = String(value1 - value2)
+    }
+}
